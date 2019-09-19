7649c4046f792195625ef341467ab7c8666e37f1
Brian Duffy <bgd1112@gmail.com>
2019-09-18 23:09:13 -0400

    Cupid Shuffle

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,33 @@
+var stepcount = 30.0;
+
 function rightSlideClicked() {
 	// Slide to the Left! Slide to the Right!
 	var body = document.getElementById("cupid-shuffle");
 	var count = 0;
 
 	var timer = setInterval(function () {
-        if (count == 50){
+        if (count == stepcount){
             clearInterval(timer);
             body.style.marginLeft = -100 + "vw";
         }
-        newpos = -100*(count/50.0);
+        newpos = -100*(count/stepcount);
         body.style.marginLeft = newpos + "vw";
         count++;
-    }, 16);
+    }, 16.66);
+}
+
+function leftSlideClicked() {
+    // Slide to the Left! Slide to the Right!
+    var body = document.getElementById("cupid-shuffle");
+    var count = stepcount;
+
+    var timer = setInterval(function () {
+        if (count == 0){
+            clearInterval(timer);
+            body.style.marginLeft = -0 + "vw";
+        }
+        newpos = -100*(count/stepcount);
+        body.style.marginLeft = newpos + "vw";
+        count--;
+    }, 16.66);
 }
