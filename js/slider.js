b10e6ac80cc94278d34e73d551d5bda5ae675e64
Brian Duffy <bgd1112@gmail.com>
2020-01-27 13:35:11 -0500

    Remove old code

--- js/slider.js
+++ js/slider.js
@@ -3,37 +3,13 @@ var stepcount = 30.0;
 function slideToTheRight() {
 	// Slide to the Right!
     body = document.getElementById("cha-cha-slide");
-    body.style = "margin-left: -100vw;"
-	/*var body = document.getElementById("cha-cha-slide");
-	var count = 0;
-
-	var timer = setInterval(function () {
-        if (count == stepcount){
-            clearInterval(timer);
-            body.style.marginLeft = -100 + "vw";
-        }
-        newpos = -100*(count/stepcount);
-        body.style.marginLeft = newpos + "vw";
-        count++;
-    }, 16.66);*/
+    body.style = "margin-left: -100vw;";
 }
 
 function slideToTheLeft() {
     // Slide to the Left!
     body = document.getElementById("cha-cha-slide");
-    body.style = "margin-left: 0vw;"
-    /*var body = document.getElementById("cha-cha-slide");
-    var count = stepcount;
-
-    var timer = setInterval(function () {
-        if (count == 0){
-            clearInterval(timer);
-            body.style.marginLeft = -0 + "vw";
-        }
-        newpos = -100*(count/stepcount);
-        body.style.marginLeft = newpos + "vw";
-        count--;
-    }, 16.66);*/
+    body.style = "margin-left: 0vw;";
 }
 
 function chaChaRealSmooth() {
